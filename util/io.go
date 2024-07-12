73dd683a7baa72c834f2e5e7c4abcdaa20b6a1d3
zen <zhangyiming748@gmail.com>
2024-07-12 13:48:55 +0800

    读文件方式不生效

--- util/io.go
+++ util/io.go
@@ -1,6 +1,7 @@
 package util
 
 import (
+	"bufio"
 	"os"
 	"path/filepath"
 	"strings"
@@ -9,6 +10,7 @@ import (
 type FileInfo struct {
 	FullPath  string // 文件的绝对路径
 	FullName  string // 完整的文件名
+	PurgePath string // 纯路径名 带有最后一个路径分隔符
 	PurgeName string // 相对路径的纯文件名
 	PurgeExt  string // 不带.的扩展名
 }
@@ -23,6 +25,7 @@ func GetAllFiles(p string) *[]FileInfo {
 		var fi FileInfo
 		fi.FullPath = file
 		fi.FullName = filepath.Base(file)
+		fi.PurgePath = strings.Trim(fi.FullPath, fi.FullName)
 		ext := filepath.Ext(fi.FullName)
 		fi.PurgeName = strings.TrimSuffix(fi.FullName, ext)
 		fi.PurgeExt = strings.Replace(ext, ".", "", 1)
@@ -42,4 +45,22 @@ func getAllFiles(dirPath string) ([]string, error) {
 		return nil
 	})
 	return files, err
+}
+
+/*
+按行读文件
+*/
+func ReadByline(fp string) ([]string, error) {
+	file, err := os.Open(fp)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	var lines []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	return lines, scanner.Err()
 }
